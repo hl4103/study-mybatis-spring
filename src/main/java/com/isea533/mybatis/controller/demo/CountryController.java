50ce66327db1740647a5bcf076e4b26830433fbd
何龙 <helong_yg@yun-gui.com>
2017-04-12 09:57:11 +0800

    测试

--- src/main/java/com/isea533/mybatis/controller/demo/CountryController.java
+++ src/main/java/com/isea533/mybatis/controller/demo/CountryController.java
@@ -1,8 +1,10 @@
 package com.isea533.mybatis.controller.demo;
 
-import com.github.pagehelper.PageInfo;
-import com.isea533.mybatis.model.Country;
-import com.isea533.mybatis.service.CountryService;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import com.github.pagehelper.PageInfo;
+import com.isea533.mybatis.model.Country;
+import com.isea533.mybatis.service.CountryService;
 
 /**
  * @author liuzh_3nofxnp
@@ -37,6 +41,14 @@ public class CountryController {
         result.addObject("page", page);
         result.addObject("rows", rows);
         return result;
+    }
+
+	@RequestMapping(value = "test", method = RequestMethod.GET)
+	public void exampleTest(HttpServletRequest request, HttpServletResponse response, Country country) {
+		int page = 1;
+		int rows = 10;
+		List<Country> countryList = countryService.selectByCountry(country, page, rows);
+		System.out.println(countryList.toArray());
     }
 
     @RequestMapping(value = "view", method = RequestMethod.GET)
